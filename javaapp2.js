4af9b5886a053585fa2a58504b672c79702bdb23
Talafhamohammad <joseph.telfah@gmail.com>
2021-03-21 20:16:00 +0200

    final work lab 05

--- javaapp2.js
+++ javaapp2.js
@@ -7,3 +7,38 @@ if (r == true) {
 } else {
   txt = "you are welcome anytime";
 }
+var reminder = prompt("plz dont forget to provide us with your full name and phone number")
+alert("you are welcome " + reminder)
+alert("and dont forget o chose your colour from above")
+
+var welCome = prompt("you are welcomed here can you tell us which one of these is your fav place[PETRA],[JERASH],[AQABA] ?");
+
+while (welCome !== 'petra' && welCome !== 'jerash' && welCome !== 'aqaba') {
+
+  welCome = prompt('please choose petra ,jerash or aqaba');
+}
+
+var numberOfphotos = prompt('chose the number of your photos')
+
+var photo = ''
+
+var counter = ''
+
+if (welCome == 'petra') {
+  photo = ('<img src="petra.jpg"/> </br>');
+}
+else if (welCome == 'jerash') {
+  photo = ('<img src="jerash.jpg"/> </br>');
+}
+else {
+  photo = ('<img src="aqaba.jpg"/> </br>');
+}
+
+for (i = 0; i < numberOfphotos; i++) {
+  counter += photo;
+
+}
+document.write(counter)
+
+
+
